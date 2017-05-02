b50abb38503682cec37bf5a8e769c0570c961b26
Michael Loubier <michael.a.loubier@gmail.com>
2017-05-02 15:08:22 -0700

    Redid binary and linear iterative and recursive search for practice.

--- source/search.py
+++ source/search.py
@@ -1,10 +1,12 @@
 #!python
+import math
+
 
 def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    return linear_search_iterative(array, item)
+    return linear_search_recursive(array, item)
     # return linear_search_recursive(array, item)
 
 
@@ -18,28 +20,58 @@ def linear_search_iterative(array, item):
 
 def linear_search_recursive(array, item, index=0):
     # TODO: implement linear search recursively here
-    pass
-    # once implemented, change linear_search to call linear_search_recursive
-    # to verify that your recursive implementation passes all tests below
+    if index >= len(array):
+        return None
+    elif array[index] == item:
+        return index
+    else:
+        return linear_search_recursive(array, item, index+1)
 
 
 def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
     # implement binary_search_iterative and binary_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    return binary_search_iterative(array, item)
+    return binary_search_recursive(array, item)
     # return binary_search_recursive(array, item)
 
 
 def binary_search_iterative(array, item):
     # TODO: implement binary search iteratively here
-    pass
-    # once implemented, change binary_search to call binary_search_iterative
-    # to verify that your iterative implementation passes all tests below
+    curr_index = len(array)/2
+    last_index = 0
+    curr_value = array[curr_index]
+    while curr_value != item:
+        new_index = int(math.ceil(abs(curr_index - last_index) / 2.0))
+        if curr_value < item:
+            new_index = curr_index + new_index
+        else:
+            new_index = curr_index - new_index
+        if curr_index <= 0 or curr_index >= len(array) - 1 or new_index == last_index:
+            return None
+        last_index = curr_index
+        curr_index = new_index
+        curr_value = array[curr_index]
+    return curr_index
 
 
 def binary_search_recursive(array, item, left=None, right=None):
     # TODO: implement binary search recursively here
-    pass
+    if left is None:
+        left = 0
+    if right is None:
+        right = len(array)
+    print("Right: ", right, "Left: ", left)
+    incrementor = (right - left) / 2
+    index = left + incrementor
+    if index < 0 or index > len(array) - 1 or index - 1 == right or index + 1 == left:
+        return None
+    if array[index] == item:
+        print("index", index)
+        return index
+    elif array[index] > item:
+        return binary_search_recursive(array, item, left, index - 1)
+    elif array[index] < item:
+        return binary_search_recursive(array, item, index + 1, right)
     # once implemented, change binary_search to call binary_search_recursive
     # to verify that your recursive implementation passes all tests below
--- work/recursive_binary_search.py
+++ work/recursive_binary_search.py
@@ -7,26 +7,26 @@ def recursive_binary_search(input_list, target, iterations=0, curr_index=0):
     iterations += 1
     # if curr_index is None:
     #     curr_index = len(input_list)//2
-    print("Iterations: ", iterations, math.ceil(len(input_list)/(2**iterations)), " curr_index: ", curr_index)
+    # print("Iterations: ", iterations, math.ceil(len(input_list)/(2**iterations)), " curr_index: ", curr_index)
     if curr_index >= len(input_list) or iterations > len(input_list):
-        print("Target not found.")
+        # print("Target not found.")
         return None
     elif input_list[curr_index] == target:
-        print("On target!")
-        print(curr_index)
+        # print("On target!")
+        # print(curr_index)
         return curr_index
     else:
         # print("Step:", len(input_list)//iterations)
         if input_list[curr_index] < target:
-            print("Less than target.")
-            new_index = curr_index + math.ceil(len(input_list)/(2**iterations))
-            print(new_index)
+            # print("Less than target.")
+            new_index = curr_index + int(math.ceil(len(input_list)/(2**iterations)))
+            # print(new_index)
             # print(input_list[new_index])
             return recursive_binary_search(input_list, target, iterations, new_index)
         else:
-            print("Greater than target.")
-            new_index = curr_index - math.ceil(len(input_list)/(2**iterations))
-            print(new_index)
+            # print("Greater than target.")
+            new_index = curr_index - int(math.ceil(len(input_list)/(2**iterations)))
+            # print(new_index)
             # print(input_list[new_index])
             return recursive_binary_search(input_list, target, iterations, new_index)
 
